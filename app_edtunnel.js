c37aa39054f16362d15bd9b71d60c80a8ef9f18b
zouludaifengderongrong <142240319+zouludaifengderongrong@users.noreply.github.com>
2023-08-20 15:26:33 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,8 +4,8 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
-let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
+let userID = '5aead4e4-1a01-4e01-82a9-6e379e50c8ec';
+let proxyIP = "63.68.197." + Math.floor(Math.random() * 255);
 
 let address = '';
 let portWithRandomLog = '';
